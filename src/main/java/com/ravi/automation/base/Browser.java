e7b37627cdc2966af7fe61c59ab5531e9e4d2f2f
Ravi Pratap <pratapravi1408@gmail.com>
2019-09-29 15:26:29 +0530

    build trigger test 1

--- src/main/java/com/ravi/automation/base/Browser.java
+++ src/main/java/com/ravi/automation/base/Browser.java
@@ -38,9 +38,12 @@ public class Browser {
 		return driver;
 	}
 	/*
-	 * WebDriver eventDriver() { WebDriver driver; EventFiringWebDriver ef_Driver =
-	 * new EventFiringWebDriver(this.driver); WebEventListners webEventListners =
-	 * new WebEventListners(test); ef_Driver.register(webEventListners); driver =
-	 * ef_Driver; return driver; }
+	 WebDriver eventDriver() { 
+	 	WebDriver driver; 
+	 	EventFiringWebDriver ef_Driver = new EventFiringWebDriver(this.driver); 
+	 	WebEventListners webEventListners = new WebEventListners(test); 
+	 	ef_Driver.register(webEventListners); 
+	 	driver = ef_Driver; return driver; 
+	 }
 	 */
 }
